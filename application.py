48ec4dbf8bea07e650b77b5337949b52217f5cd7
구현민 <62460314+hminkoo10@users.noreply.github.com>
2023-05-25 16:40:09 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,13 +1,12 @@
 from flask import Flask, jsonify, request
-import requests
-import json
+import requests,random,json,datetime,urllib3
 from bs4 import BeautifulSoup
-import re
-import datetime
-import urllib3
-from neispy import Neispy
-import asyncio
+from twilio.rest import Client
 
+pnt = {}
+TWILIO_ACCOUNT_SID = '비공개'
+TWILIO_AUTH_TOKEN = '비공개'
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 dt = datetime.datetime
 application = Flask(__name__)
@@ -90,10 +89,9 @@ def get_diet(date,sc,n=0):
     except:
         meal = "급식이 없어요"
         return meal
-    mscu = "https://open.neis.go.kr/hub/mealServiceDietInfo"
-    mscu = f"https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE={gccode}&SD_SCHUL_CODE={sccode}"
+    mscu = f"https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=비공개&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE={gccode}&SD_SCHUL_CODE={sccode}"
     mpara = {
-        "KEY": "",
+        "KEY": "비공개",
         "Type": "json",
         "pIndex":1,
         "pSize":100,
@@ -110,73 +108,62 @@ def get_diet(date,sc,n=0):
         meal = "급식이 없어요"
     return meal
 
-class SchoolApi:
-
-    params = {
-        "KEY": "",
+def get_time(date,sc,grade,class_,n=0):
+    scu = "https://open.neis.go.kr/hub/schoolInfo"
+    para = {
+        "KEY": None,
         "Type": "json",
+        "pIndex": None,
+        "pSize": None,
+        "ATPT_OFCDDC_SC_CODE": None,
+        "SD_SCHUL_CODE": None,
+        "SCHUL_NM": sc,
+        "SCHUL_KND_SC_NM": None,
+        "LCTN_SC_NM": None,
+        "FOND_SC_NM": None,
     }
-
-    schoolinfo = {}
-
-    base_url = "https://open.neis.go.kr/hub/"
-    def __init__(self, sub_url, params):
-        self.sub_url = sub_url
-        self.params = params
-    def get_data(self):
-        URL = SchoolApi.base_url + self.sub_url
-        self.params.update(SchoolApi.params)
-        self.params.update(SchoolApi.schoolinfo)
-        response = requests.get(URL, params=self.params)
-
-        try:
-            j_response = json.loads(response.text)[self.sub_url]
-            if j_response[0]["head"][0]["list_total_count"] == 1:
-                return j_response[1]["row"][0]
-            else:
-                return j_response[1]["row"]
-        except:
-            print("찾는 데이터가 없습니다.")
-            return response.text
-    def meal(self):
-        data = self.get_data()
-        try:
-            string = "<조식>\n"+data[0]["DDISH_NM"].replace("<br/>", "\n")+"\n\n"
-            string+= "<중식>\n"+data[1]["DDISH_NM"].replace("<br/>", "\n")+"\n\n"
-            string += "<석식>\n" + data[2]["DDISH_NM"].replace("<br/>", "\n")
-            characters = "1234567890./-*"
-            for x in range(len(characters)):
-                string = string.replace(characters[x],"")
-            return string
-        except:
-            return "오늘은 급식이 없습니다."
-    def time(self):
-        data = self.get_data()
-        string = ""
-        try:
-            for i in data:
-                string += i["ITRT_CNTNT"] + "\n"
-            return string
-        except:
-            return "오늘은 시간표가 없습니다."
-
-    def schedule(self):
-        data = self.get_data()
-        try:
-            return "오늘은 "+data["EVENT_NM"]+"이(가) 있습니다."
-        except:
-            return "x"
-
-    def get_school_info(self):
-        data = self.get_data()
-        try:
-            SchoolApi.schoolinfo = {
-            "ATPT_OFCDC_SC_CODE": data["ATPT_OFCDC_SC_CODE"],
-            "SD_SCHUL_CODE": data["SD_SCHUL_CODE"]
-            }
-        except:
-            print("학교를 못 찾겠어요")
-
+    res = requests.get(url=scu, params=para, verify=False, json=True)
+    res.encoding = "UTF-8"
+    rj = res.json()
+    try:
+        sccode = rj["schoolInfo"][1]["row"][n]["SD_SCHUL_CODE"]
+        gccode = rj["schoolInfo"][1]["row"][n]["ATPT_OFCDC_SC_CODE"]
+    except:
+        meal = "시간표가 없어요"
+        return meal
+    if rj["schoolInfo"][1]["row"][n]["SCHUL_NM"].endswith("초등학교"):
+        mscu = f"https://open.neis.go.kr/hub/elsTimetable?KEY=비공개&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE={gccode}&SD_SCHUL_CODE={sccode}"
+        tb = "elsTimetable"
+    elif rj["schoolInfo"][1]["row"][n]["SCHUL_NM"].endswith("중학교"):
+        mscu = f"https://open.neis.go.kr/hub/misTimetable?KEY=비공개&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE={gccode}&SD_SCHUL_CODE={sccode}"
+        tb = "misTimetable"
+    elif rj["schoolInfo"][1]["row"][n]["SCHUL_NM"].endswith("고등학교"):
+        mscu = f"https://open.neis.go.kr/hub/hisTimetable?KEY=비공개&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE={gccode}&SD_SCHUL_CODE={sccode}"
+        tb = "hisTimetable"
+    mpara = {
+        "KEY": "비공개",
+        "Type": "json",
+        "pIndex":1,
+        "pSize":100,
+        "ATPT_OFCDDC_SC_CODE": gccode,
+        "SD_SCHUL_CODE": sccode,
+        "TI_FROM_YMD": date,
+        "TI_TO_YMD": date,
+        "GRADE": grade,
+        "CLASS_NM": class_
+    }
+    mres = requests.get(url=mscu, params=mpara, verify=False, json=True)
+    mres.encoding = "UTF-8"
+    mrj = mres.json()
+    try:
+        tt = ""
+        t = mrj[tb][0]["head"][0]["list_total_count"]
+        for i in range(t):
+            tn = mrj[tb][1]["row"][i]["ITRT_CNTNT"].replace("-","")
+            tt += f"{i+1}교시 : {tn}\n"
+    except Exception as e:
+        return str(e)
+    return tt
 
 @application.route("/webhook/", methods=["POST"])
 def webhook():
@@ -198,10 +185,10 @@ def get_question():
     a[request_data['userRequest']['user']['id']] = '아직 AI가 처리중이에요. 다시 답변을 입력 해 주세요'
     try:
         api = requests.post('https://api.asyncia.com/v1/api/request/', json={
-            "apikey": "",
+            "apikey": "비공개",
             "messages" :[{"role": "user", "content": request_data['action']['params']['question']}],
             "userdata": [["user", request_data['userRequest']['user']['id']]]},
-            headers={"apikey":""}, timeout=0.3)
+            headers={"apikey":"비공개"}, timeout=0.3)
     except requests.exceptions.ReadTimeout:
         pass
     with open("bot.log", "a+") as f:
@@ -247,37 +234,121 @@ def meal():
         f.write(f"\n\n\n\n\n{rd['userRequest']['user']['id']} : 급식 명령어를 입력함\n{q}\n{diet}")
     return jsonify(response)
 
-@application.route("/scinf",methods=["POST"])
-def scinf():
+@application.route("/time",methods=["POST"])
+def time():
     rd = json.loads(request.get_data(),encoding='utf-8')
-    if rd['action']['params']['question'] != "학교이름":
-        response = { "version": "2.0", "template": { "outputs": [{
-        "simpleText": {"text": f"현재 경기도 교육청 일부 학교만 지원하고 있습니다."}
-    }]}}
-        with open("bot.log", "a+") as f:
-            f.write(f"\n\n\n\n\n{rd['userRequest']['user']['id']} : 오늘 명령어를 사용함 (다른학교 입력해 접근거부)")
-        return jsonify(response)
-    params = {
-        "SCHUL_NM": rd['action']['params']['question'] + "학교"
-    }
-    SchoolApi("schoolInfo", params).get_school_info()
-    msg=""
-    params = {"MLSV_YMD": dt.now().strftime("%Y%m%d")}
-    msg+="[ 오늘의 급식 ]\n"
-    msg+=SchoolApi("mealServiceDietInfo", params).meal()
-    params = {"GRADE": "1", "CLASS_NM": "3", "ALL_TI_YMD": dt.now().strftime("%Y%m%d")}
-    msg+="\n[ 오늘의 시간표 ]\n"
-    msg+=SchoolApi("hisTimetable", params).time()
-    if(SchoolApi("SchoolSchedule", params).schedule()!="x"):
-        msg+="[ 오늘의 학사일정 ]\n"
-        msg+=SchoolApi("SchoolSchedule", params).schedule()
     response = { "version": "2.0", "template": { "outputs": [{
-        "simpleText": {"text": msg}
+        "simpleText": {"text": f"현재 점검중"}
+    }]}}
+    #return jsonify(response)
+    q = rd['action']['params']['question'].replace(" ","")
+    n = q.split("#")
+    n = q.split("#")
+    c = rd['action']['params']['class'].replace(" ","")
+    try:
+        c1 = c.split("-")[0]
+        c2 = c.split("-")[1]
+    except:
+        response = { "version": "2.0", "template": { "outputs": [{
+            "simpleText": {"text": f"학년/반을 정확히 -로 구분해서 말해주세요"}
+        }]}}
+        return jsonify(response)
+    if len(n) == 1:
+        n.append(1)
+    reg = get_school_info(n[0],int(n[1])-1)
+    if reg == "없는 학교에요":
+        response = { "version": "2.0", "template": { "outputs": [{
+            "simpleText": {"text": f"없는 학교에요"}
+        }]}}
+        return jsonify(response)
+    diet = f"이번주 {reg['지역']} {reg['한글이름']} 시간표입니다"
+    today = datetime.datetime.today()
+    last_monday = today - datetime.timedelta(days = today.weekday())
+    for i in range(5):
+        gi = (last_monday + datetime.timedelta(days = i)).strftime("%Y%m%d")
+        diet = f"{diet}\n\n[ {(last_monday + datetime.timedelta(days = i)).strftime('%Y년 %m월 %d일')} {weekday[str(i+2)]} ]\n{get_time(gi,n[0],c1,c2,int(n[1])-1)}"
+    response = { "version": "2.0", "template": { "outputs": [{
+        "simpleText": {"text": diet}
     }]}}
     with open("bot.log", "a+") as f:
-        f.write(f"\n\n\n\n\n{rd['userRequest']['user']['id']} : 오늘 명령어를 사용함\n{msg}")
+        f.write(f"\n\n\n\n\n{rd['userRequest']['user']['id']} : 시간표 명령어를 입력함\n{q} {c}\n{diet}")
+    return jsonify(response)
+
+@application.route("/scinf",methods=["POST"])
+def scinf():
+    try:
+        rd = json.loads(request.get_data(),encoding='utf-8')
+        q = rd['action']['params']['question'].replace(" ","")
+        n = q.split("#")
+        c = rd['action']['params']['class'].replace(" ","")
+        try:
+            c1 = c.split("-")[0]
+            c2 = c.split("-")[1]
+        except:
+            response = { "version": "2.0", "template": { "outputs": [{
+                "simpleText": {"text": f"학년/반을 정확히 -로 구분해서 말해주세요"}
+            }]}}
+            return jsonify(response)
+        if len(n) == 1:
+            n.append(1)
+        reg = get_school_info(n[0],int(n[1])-1)
+        if reg == "없는 학교에요":
+            response = { "version": "2.0", "template": { "outputs": [{
+                "simpleText": {"text": f"없는 학교에요"}
+            }]}}
+            return jsonify(response)
+        gi = datetime.datetime.now().strftime("%Y%m%d")
+        msg=""
+        msg+="[ 오늘의 급식 ]\n"
+        msg+=get_diet(gi,n[0],int(n[1])-1)
+        msg += "\n"
+        msg+="\n[ 오늘의 시간표 ]\n"
+        msg+=get_time(gi,n[0],c1,c2,int(n[1])-1)
+        #if(SchoolApi("SchoolSchedule", params).schedule()!="x"):
+        #    msg+="[ 오늘의 학사일정 ]\n"
+        #    msg+=SchoolApi("SchoolSchedule", params).schedule()
+        response = { "version": "2.0", "template": { "outputs": [{
+            "simpleText": {"text": msg}
+        }]}}
+        with open("bot.log", "a+") as f:
+            f.write(f"\n\n\n\n\n{rd['userRequest']['user']['id']} : 오늘 명령어를 사용함\n{msg}")
+        return jsonify(response)
+    except Exception as e:
+        response = { "version": "2.0", "template": { "outputs": [{
+            "simpleText": {"text": str(e)}
+        }]}}
+        return jsonify(response)
+
+@application.route("/register",methods=["POST"])
+def register():
+    rd = json.loads(request.get_data(),encoding='utf-8')
+    response = { "version": "2.0", "template": { "outputs": [{
+            "simpleText": {"text": "이 기능은 현재 점검중입니다"}
+        }]}}
+    return jsonify(response)
+    if rd['userRequest']['user']['id'] in pnt.keys():
+        response = { "version": "2.0", "template": { "outputs": [{
+            "simpleText": {"text": "이미 가입 요청을 했습니다\n문의 : https://open.kakao.com/me/hyunminkoo"}
+        }]}}
+        with open("bot.log", "a+") as f:
+            f.write(f"\n\n\n\n\n{rd['userRequest']['user']['id']} : 가입 재요청 했으므로 입구컷")
+        return jsonify(response)
+    n = '\n금쪽이 가입 인증번호\n'
+    for i in range(8):
+        n += str(random.randint(0,9))
+    client.messages.create(
+        to=f"+82{rd['action']['params']['phone']}",
+        from_="비공개",
+        body=n
+    )
+    pnt[rd['userRequest']['user']['id']]  = (n,rd['action']['params']['phone'],rd['action']['params']['name'])
+    response = { "version": "2.0", "template": { "outputs": [{
+        "simpleText": {"text": "휴대전화로 인증번호 8자리를 보내드렸어요! '인증' 명령어로 인증번호를 입력 해 주세요!"}
+    }]}}
+    with open("bot.log", "a+") as f:
+        f.write(f"\n\n\n\n\n{rd['userRequest']['user']['id']} : 가입신청함\n이름 : {rd['action']['params']['name']}\n전화번호 : {rd['action']['params']['phone']}")
     return jsonify(response)
 
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=80, debug=True)
+    application.run(host='0.0.0.0', port=80, debug=False)
